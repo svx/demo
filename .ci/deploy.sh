c60be2b96db58ff0d57da930c78d117861840c01
sven <sven@so36.net>
2019-03-12 09:17:48 +0100

    [ci] Update name of site

--- .ci/deploy.sh
+++ .ci/deploy.sh
@@ -3,18 +3,11 @@ set -e # halt script on error
 
 zip -r website.zip site
 
-#curl -H "Content-Type: application/zip" \
-#     -H "Authorization: Bearer $NETLIFYKEY" \
-#     --data-binary "@website.zip" \
-#     https://api.netlify.com/api/v1/sites/$API/deploys
 
-NETLIFY_SITE_NAME=musing-raman-fd789e.netlify.com
+NETLIFY_SITE_NAME=sleepy-snyder-1f3b01.netlify.com
 
 curl -H "Content-Type: application/zip" \
      -H "Authorization: Bearer $NETLIFY_PUBLISH_Key" \
      --data-binary "@website.zip" \
      https://api.netlify.com/api/v1/sites/$NETLIFY_SITE_NAME/deploys
 
-# https://www.netlify.com/docs/api/#deploys
-#echo "Publishing Netlify build ${good_deploy}..."
-#curl -X POST -H "Authorization: Bearer $NETLIFYKEY" -d "{}" "https://api.netlify.com/api/v1/sites/continuous-sphinx.netlify.com/deploys/${good_deploy}/restore"
